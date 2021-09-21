784fd172c469982dcd726626b833a852d67509df
David Poole <14233215+davidlpoole@users.noreply.github.com>
2021-09-21 13:16:28 +1200

    add login route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+const loginRouter = require('./controllers/login')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
 
@@ -31,6 +32,7 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 
